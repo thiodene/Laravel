f526b3fc7d716254d1aac6f84aef0328e321bedc
Serge Ayissi <ayissi.serge@gmail.com>
2019-10-03 14:49:16 -0400

    Update install_laravel.sh

--- install_laravel.sh
+++ install_laravel.sh
@@ -1,3 +1,23 @@
+# Install Laravel right after Composer is installed and ready
+#download as superuser
+#download installer
+sudo composer global require "laravel/installer=~1.1"
+
+# setting up path
+# Open .bashrc and Edit it
+sudo vi ~/.bashrc
+# Add the following line to at the end of it:
+export PATH="$PATH:~/.composer/vendor/bin"
+# Or this line
+export PATH="~/.composer/vendor/bin:$PATH"
+
+# Now restart .bashrc
+source ~/.bashrc
+
+# Now type Laravel to see if it executes on the command line
+laravel
+
+# Old way---------------------------------------------------------------------------
 # Source:
 # https://www.tecmint.com/install-laravel-php-framework-on-ubuntu/
 cd /var/www/html
